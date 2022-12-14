d428edfa294f76b992b7b4d4ad8fc5897b5a93db
Nitesh <Nitesh@Lenovo-Nitesh>
2022-12-14 17:47:56 +0530

    done

--- Servlets-Banking/src/main/java/com/Banking/LoginServlet.java
+++ Servlets-Banking/src/main/java/com/Banking/LoginServlet.java
@@ -1,16 +1,67 @@
 package com.Banking;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-public class LoginServlet  extends HttpServlet{
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String email = req.getParameter("email");
+		long mobile = Long.parseLong(req.getParameter("pws"));
 		
+		PrintWriter pw = resp.getWriter();
+		pw.println(mobile);
+		HttpSession hs = req.getSession();
+		//setter session
+		hs.setAttribute("email", email);
+		hs.setAttribute("mobile", mobile);
+		
+		HttpSession hs1 =  req.getSession();
+		
+		// getter session
+		System.out.println((String)hs.getAttribute("email"));
+		System.out.println((Long)hs.getAttribute("mobile"));
+		
+//		// remove session
+//		hs.removeAttribute("email");
+//
+//		System.out.println((String)hs.getAttribute("email"));
+//		System.out.println((Long)hs.getAttribute("mobile"));
+//		
+//		// invalidate session
+//		hs.invalidate();
+//		
+//		System.out.println((String)hs.getAttribute("email"));
+//		System.out.println((Long)hs.getAttribute("mobile"));
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the email:");
+		String usr = sc.next();
+		
+		System.out.println("Enter the password:");
+		
+		long pws = sc.nextLong();
+		
+		
+		if(email.equals(usr)|| pws==mobile) {
+			
+			
+			pw.println("Successfully Login.......\n thanks for login....");
+			
+			RequestDispatcher rd =req.getRequestDispatcher("login.html");
+			rd.include(req, resp);
+		}
 	}
 }
---
+++ Servlets-Banking/src/main/java/com/Banking/signup.java
@@ -0,0 +1,21 @@
+package com.Banking;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class signup  extends HttpServlet{
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		String name = req.getParameter("name");
+		String email =  req.getParameter("name");
+		long mobile =  Long.parseLong(req.getParameter("mobile"));
+		long adhar = req.getParameter(); 
+	}
+
+}
